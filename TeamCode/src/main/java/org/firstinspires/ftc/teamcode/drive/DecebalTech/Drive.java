014ed8e01e52866d7dd93a6343a760c4fde79cbc
N0bmaster <mihutmatei@gmail.com>
2023-11-02 14:05:44 +0200

    fix 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DecebalTech/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DecebalTech/Drive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.drive.DecebalTech;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.outoftheboxrobotics.photoncore.PhotonCore;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -32,7 +33,8 @@ public class Drive extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-
+        PhotonCore.experimental.setSinglethreadedOptimized(true);
+        PhotonCore.enable();
 
 
         drive = new SampleMecanumDrive(hardwareMap);
